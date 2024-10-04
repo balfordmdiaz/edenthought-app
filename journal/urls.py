12e025cb0f32c6bf8f76cf11815cd0a7bfd64592
balfordmdiaz <balfordm21@gmail.com>
2024-10-04 17:30:24 -0600

    authentication feature

--- journal/urls.py
+++ journal/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('register', views.register, name='register'),
-    path('login', views.login, name='login'),
+    path('login', views.login, name='login'), 
+    path('logout', views.user_logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,20 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from .forms import CreateUserForm, LoginForm
+
 
 # Create your views here.
 def homepage(request):
     
     return render(request, 'journal/index.html')
 
+
 def register(request):
     
-    return render(request, 'journal/register.html')
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        
+        form = CreateUserForm(request.POST)
+        
+        if form.is_valid():
+            
+            form.save()
+            
+            return redirect('login')
+        
+    context = {'RegistrationForm': form}
+    
+    return render(request, 'journal/register.html', context)
 
 
 def login(request):
     
-    return render(request, 'journal/my_login.html')
+    form = LoginForm()
+    
+    if request.method == 'POST':
+        
+        form = LoginForm(request, data=request.POST)
+        
+        if form.is_valid():
+            
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+                
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                
+                auth.login(request, user)
+                
+                return redirect('dashboard')
+    
+    context = {'LoginForm': form}
+
+    
+    return render(request, 'journal/my_login.html', context)
 
 
+def user_logout(request):
+    
+    auth.logout(request)
+    
+    return redirect("home")
+
+
+@login_required(login_url='login')
 def dashboard(request):
     
     return render(request, 'journal/dashboard.html')
