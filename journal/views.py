668febb87ff85d64d471116e15b0331c2de04ad5
balfordmdiaz <balfordm21@gmail.com>
2024-10-04 11:11:47 -0600

    creating journal app

---
+++ journal/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def homepage(request):
+    
+    pass
+
+def register(request):
+    
+    pass
+
+
+def login(request):
+    
+    pass
+
+
+def dashboard(request):
+    
+    pass
