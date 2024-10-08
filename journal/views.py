110ef13c708f5b58cece286c0815f2b726431e79
balfordmdiaz <balfordm21@gmail.com>
2024-10-07 21:11:15 -0600

    setting up the styles of the app

--- journal/views.py
+++ journal/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import CreateUserForm, LoginForm
 
@@ -23,6 +24,8 @@ def register(request):
         if form.is_valid():
             
             form.save()
+            
+            messages.success(request, "User Created!")
             
             return redirect('login')
         
